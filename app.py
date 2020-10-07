fae9ccf65665d94a34dc7bac8e56d4f21ce9ec0d
Kathirvel Chandrasekaran <kathirvel98@outlook.com>
2020-10-07 11:19:38 +0530

    Added endpoints

--- app.py
+++ app.py
@@ -57,5 +57,35 @@ def get_questions():
     return jsonify({'result': results})
 
 
+@app.route('/api/questions/tag/<tagName>', methods=["GET"])
+def get_tagDetails(tagName):
+    questions = mongo.db.posts
+    count = 0
+    results = []
+    for i in questions.find({"post_tag": tagName}):
+        results.append({
+            "question": i["question"],
+            "views": i["views"],
+            "vote_count": i["vote_count"],
+            "post_tag": i["post_tag"],
+            "asked_by": i["asked_by"],
+            "avatar": i["avatar"],
+        })
+        count += 1
+
+    print("Total count", count)
+    return jsonify({'result': results})
+
+
+@app.route('/api/questions/tagcount/<tagName>', methods=["GET"])
+def get_tagCount(tagName):
+    questions = mongo.db.posts
+    count = 0
+    for i in questions.find({"post_tag": tagName}):
+        count += 1
+
+    return jsonify({'tag-count': count})
+
+
 if __name__ == '__main__':
     app.run()
